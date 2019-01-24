ccd63e3b034bbfd0076b8184432cfdec8014c37e
sasaplus1 <sasaplus1@gmail.com>
2019-01-24 18:13:20 +0900

    updated index.js

--- index.js
+++ index.js
@@ -6,30 +6,29 @@ module.exports = function() {
   const newlines = {};
 
   return {
-    code: function(input, filepath) {
+    code(input, filepath) {
       if (!frontMatter.test(input)) {
         newlines[filepath] = 0;
 
         return input;
       }
 
-      const content = frontMatter(input),
-            body = content.body;
+      const content = frontMatter(input);
+      const body = content.body;
 
       const newlineMatcher = /\r?\n/g;
 
       newlines[filepath] =
-        input.match(newlineMatcher).length -
-        body.match(newlineMatcher).length;
+        input.match(newlineMatcher).length - body.match(newlineMatcher).length;
 
       return body;
     },
-    result: function(stylelintResult, filepath) {
+    result(stylelintResult, filepath) {
       stylelintResult.warnings.forEach(function(warning) {
         warning.line += newlines[filepath];
       });
 
       return stylelintResult;
-    },
+    }
   };
 };
