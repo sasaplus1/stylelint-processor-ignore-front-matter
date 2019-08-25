25b5c79e8dab669ea1d335ef05c9e36f823e49f6
sasaplus1 <sasaplus1@gmail.com>
2019-08-25 21:44:24 +0900

    update lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,10 +4,6 @@ module.exports = {
     'npx prettier --parser yaml --write',
     'git diff --exit-code --quiet'
   ],
-  '!(package).json': [
-    'npx prettier --parser json-stringify --write',
-    'git diff --exit-code --quiet'
-  ],
   'package.json': [
     'npx fixpack',
     'npx prettier --parser json-stringify --write',
