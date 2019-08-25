b41e8f39e5a55d82be4554c60fae79e3e125733c
sasaplus1 <sasaplus1@gmail.com>
2019-08-25 21:29:52 +0900

    migrate .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-  linters: {
-    '*.js': 'npx --no-install eslint',
-    '*.yml': [
-      'npx --no-install prettier --parser yaml --write',
-      'git diff --exit-code --quiet'
-    ],
-    '!(package|package-lock).json': [
-      'npx --no-install prettier --parser json-stringify --write',
-      'git diff --exit-code --quiet'
-    ],
-    'package.json': [
-      'npx fixpack',
-      'npx --no-install prettier --parser json-stringify --write',
-      'git diff --exit-code --quiet'
-    ]
-  }
+  '*.js': 'npx eslint',
+  '*.yml': [
+    'npx prettier --parser yaml --write',
+    'git diff --exit-code --quiet'
+  ],
+  '!(package).json': [
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package.json': [
+    'npx fixpack',
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package-lock.json': 'node -e "process.exit(1);"'
 };
