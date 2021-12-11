2c57a113c446138554b836fd3bddf681165543cd
sasaplus1 <sasaplus1@gmail.com>
2021-12-11 22:33:48 +0900

    simplify

--- test/index.js
+++ test/index.js
@@ -10,20 +10,6 @@ const tmp = require('tmp');
 const tempFile = util.promisify(tmp.file);
 const writeFile = util.promisify(fs.writeFile);
 
-function lint(files) {
-  const options = Object.assign({
-    config: {
-      processors: path.resolve(__dirname, '../index.js'),
-      rules: {
-        'block-no-empty': true
-      }
-    },
-    files: path.resolve(__dirname, files)
-  });
-
-  return stylelint.lint(options);
-}
-
 async function createFile(text) {
   const file = await tempFile({ postfix: '.css' });
 
@@ -33,38 +19,61 @@ async function createFile(text) {
 }
 
 describe('index.js', function () {
-  let pattern = 1;
+  const options = {
+    config: {
+      processors: path.resolve(__dirname, '../index.js'),
+      rules: {
+        'block-no-empty': true
+      }
+    }
+  };
 
   it("don't throw an error", async function () {
     const file = await createFile(['---', '---'].join('\n'));
-    const { errored } = await lint(file);
+
+    const { errored } = await stylelint.lint({
+      ...options,
+      files: file
+    });
 
     assert(errored === false);
   });
-  it(`pattern ${pattern++}`, async function () {
+  it('has not EOL', async function () {
     const file = await createFile(['---', '---', 'body {}'].join('\n'));
-    const result = await lint(file);
+    const result = await stylelint.lint({
+      ...options,
+      files: file
+    });
     const warning = get(result, 'results[0].warnings[0]');
 
     assert(warning.line === 3);
   });
-  it(`pattern ${pattern++}`, async function () {
+  it('has EOL', async function () {
     const file = await createFile(['---', '---', 'body {}', ''].join('\n'));
-    const result = await lint(file);
+    const result = await stylelint.lint({
+      ...options,
+      files: file
+    });
     const warning = get(result, 'results[0].warnings[0]');
 
     assert(warning.line === 3);
   });
-  it(`pattern ${pattern++}`, async function () {
+  it('LF before selector', async function () {
     const file = await createFile(['---', '---', '', 'body {}'].join('\n'));
-    const result = await lint(file);
+    const result = await stylelint.lint({
+      ...options,
+      files: file
+    });
     const warning = get(result, 'results[0].warnings[0]');
 
     assert(warning.line === 4);
   });
-  it(`pattern ${pattern++}`, async function () {
+  it('has Front-matter', async function () {
     const file = await createFile(['---', '', '---', '', 'body {}'].join('\n'));
-    const result = await lint(file);
+    const result = await stylelint.lint({
+      ...options,
+      files: file
+    });
     const warning = get(result, 'results[0].warnings[0]');
 
     assert(warning.line === 5);
