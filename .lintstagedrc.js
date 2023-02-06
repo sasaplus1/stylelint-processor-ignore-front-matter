5a5f92e940ba0db4476bc40ec2da0629d2910810
sasaplus1 <sasaplus1@gmail.com>
2023-02-06 16:34:58 +0900

    migrate to npm from yarn

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,5 @@ module.exports = {
   '*.js': ['eslint', 'prettier --check'],
   '*.+(md|yml)': 'prettier --check',
   '!(package|package-lock).json': 'prettier --check',
-  'package.json': ['npx fixpack --dryRun', 'prettier --check'],
-  'package-lock.json': 'node -e "process.exitCode = 1;"'
+  'package.json': ['npx fixpack --dryRun', 'prettier --check']
 };
